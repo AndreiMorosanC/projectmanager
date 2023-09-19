96b95e04c467af4ee2b7e4d1c06e6eca8e63d906
AndreiMorosanC <andreimorosan39@gmail.com>
2023-09-19 17:16:21 +0300

    loggin and signUp page

---
+++ scripts/authenticationLogic.js
@@ -0,0 +1,86 @@
+// const btnSignUp = document.getElementById("btnSignUp");
+// const userData = {}
+
+// btnSignUp.addEventListener("click", () => {
+//   const nname = document.getElementById("Name").value;
+//   const surname = document.getElementById("Surname").value;
+//   const username = document.getElementById("username").value;
+//   const email = document.getElementById("email").value;
+//   const password = document.getElementById("password").value;
+//   const confirmPassword = document.getElementById("confirm-password").value;
+
+//   const userData = {
+//     name: nname,
+//     surname: surname,
+//     username: username,
+//     email: email,
+//     password: password,
+//     confirmPassword: confirmPassword,
+//   };
+
+//   console.log(userData);
+
+// });
+
+// const userDataJSON = JSON.stringify(userData);
+
+// function setCookie(name, value, daysToExpire) {
+//   const expirationDate = new Date();
+//   expirationDate.setDate(expirationDate.getDate() + daysToExpire);
+
+//   const cookieValue = `${name}=${encodeURIComponent(value)}; expires=${expirationDate.toUTCString()}; path=/`;
+//   document.cookie = cookieValue;
+// }
+
+// setCookie("userData", userDataJSON, 30);
+
+// btnSignUp.addEventListener("click", ()=>{
+//     window.location.href = '../index.html';
+// })
+// console.log(userData);
+
+const SignUpForm = document.getElementById("SignUpForm");
+const btnSignUp = document.getElementById("btnSignUp")
+
+const userData = {};
+btnSignUp.addEventListener("click", () => {
+    
+    const nname = document.getElementById("Name").value;
+    const surname = document.getElementById("Surname").value;
+    const username = document.getElementById("username").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const confirmPassword = document.getElementById("confirm-password").value;
+
+   
+    
+
+    const userData = {
+        username: username,
+        email: email,
+        password: password,
+    };
+    const userDataJSON = JSON.stringify(userData);
+        function setCookie(name, value, daysToExpire) {
+    const expirationDate = new Date();
+    expirationDate.setDate(expirationDate.getDate() + daysToExpire);
+
+    const cookieValue = `${name}=${encodeURIComponent(
+        value
+    )}; expires=${expirationDate.toUTCString()}; path=/`;
+    document.cookie = cookieValue;
+    }
+    setCookie("userData", userDataJSON, 30);
+
+    if (password === confirmPassword) {
+        
+        window.location.href = "../index.html";
+
+    }
+    document.getElementById("SignUpForm").reset();
+});
+
+
+
+
+
--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,58 @@
-const addTableComponent = document.getElementById("addTableComponent");
-const tablecontainer = document.getElementById("table-container");
+const mainContainer = document.getElementById("main-container");
+const loginContainer = document.getElementById("login-form");
 
-addTableComponent.addEventListener("click", () => {
-  const tableComponent = document.createElement("column-table");
-  tablecontainer.insertBefore(tableComponent, addTableComponent);
+const cookies = document.cookie;
+const cookieArray = cookies.split(";");
+let userData = null;
+
+for (const cookie of cookieArray) {
+  const trimmedCookie = cookie.trim();
+  if (trimmedCookie.startsWith("userData=")) {
+    const userDataString = trimmedCookie.split("=")[1];
+    userData = JSON.parse(decodeURIComponent(userDataString));
+    break;
+  }
+}
+
+
+
+
+
+
+
+const btnLoggin = document.getElementById("btnLoggin");
+
+btnLoggin.addEventListener("click", () => {
+  const password = document.getElementById("password").value;
+  const username = document.getElementById("username").value;
+
+  if (
+    userData &&
+    password === userData.password &&
+    username === userData.username
+  ) {
+    console.log("¡Inicio de sesión exitoso!");
+    password.value = "";
+    username.value = "";
+    
+    loginContainer.style.display = "none";
+    mainContainer.style.display = "grid";
+
+
+
+
+   
+  } else {
+    console.log("Nombre de usuario o contraseña incorrectos.");
+  }
 });
 
+mainContainer.addEventListener("load", () => {
+  const addTableComponent = document.getElementById("addTableComponent");
+  const tablecontainer = document.getElementById("table-container");
 
-
+  addTableComponent.addEventListener("click", () => {
+    const tableComponent = document.createElement("column-table");
+    tablecontainer.insertBefore(tableComponent, addTableComponent);
+  });
+});
